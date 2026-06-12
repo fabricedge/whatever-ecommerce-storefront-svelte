0d366bcae3e834da616c3f3af157b5711c14be40
fabricedge <sauahiglyph3@gmail.com>
2026-06-12 10:37:17 -0300

    Initial commit: SvelteKit storefront with Tailwind v4, Stripe checkout, auth, cart

--- .svelte-kit/generated/client/nodes/8.js
+++
@@ -1,3 +0,0 @@
-import * as universal from "../../../../src/routes/products/[slug]/+page.ts";
-export { universal };
-export { default as component } from "../../../../src/routes/products/[slug]/+page.svelte";
--- .svelte-kit/types/src/routes/products/[slug]/proxy+page.ts
+++
@@ -1,9 +0,0 @@
-// @ts-nocheck
-import { PUBLIC_API_URL } from '$env/static/public'
-import type { PageLoad } from './$types'
-
-export const load = async ({ params, fetch }: Parameters<PageLoad>[0]) => {
-  const res = await fetch(`${PUBLIC_API_URL}/api/products?slug=${params.slug}`)
-  const data = await res.json()
-  return { product: data.products?.[0] || null }
-}
